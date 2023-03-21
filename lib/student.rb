16ec2ca0519becda2298c723d607414ebefe55bd
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-03-21 13:46:31 -0400

    commit tree 52963b313c9816f19f6d8edddff2b04172bfff02

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,18 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+  attr_accessor :name, :grade, :id
+
+  def initialize(id, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  
+
 end
